5de2967a18dd96a1bea586d63ddc520c2344816c
Shubham Singh Chauhan <singh.shubham2297@gmail.com>
2020-08-27 03:01:43 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -138,8 +138,8 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'jk.harry2245@gmail.com'
-EMAIL_HOST_PASSWORD = 'qwerty1qwerty'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 LOGIN_URL='login'
 
